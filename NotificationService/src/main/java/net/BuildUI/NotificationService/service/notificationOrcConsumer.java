a8e4f7eb82e977633bdbf16d894b599063d58703
Dhiraj <rd6999787@gmail.com>
2024-12-26 12:08:12 +0530

    added consumer logic

--- NotificationService/src/main/java/net/BuildUI/NotificationService/service/notificationOrcConsumer.java
+++ NotificationService/src/main/java/net/BuildUI/NotificationService/service/notificationOrcConsumer.java
@@ -1,4 +1,22 @@
 package net.BuildUI.NotificationService.service;
 
+import net.BuildUI.NotificationService.models.notificationModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class notificationOrcConsumer {
+
+    @Autowired
+    private KafkaTemplate<String , notificationModel> kafkaTemplate;
+
+    @Autowired
+    private notificationService notificationService;
+
+    @KafkaListener(topics = "product_events", groupId = "notification_group")
+    private void consume(notificationModel product){
+        notificationService.processData(product);
+    }
 }
